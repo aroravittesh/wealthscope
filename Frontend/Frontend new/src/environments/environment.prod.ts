9d7ed3bbf7a844f3eb2deb0885767e020fcdcb81
Rishithaa-88 <rishithaareddy88@gmail.com>
2026-04-13 01:37:26 -0400

    Added new files with AI recommender

--- Frontend/Frontend new/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.finsight.com',
-  wsUrl: 'wss://api.finsight.com'
-};
--- Frontend/Frontend new/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: `http://${window.location.hostname}:8080/api`,
-  wsUrl: `ws://${window.location.hostname}:8080`
-};
